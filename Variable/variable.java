4c42765e3a000b39c8d2338c0159d1c777eda12f
PanditAkshay402 <PanditAkshay402@gmail.com>
2026-04-25 22:21:46 +0530

    variable

---
+++ Variable/variable.java
@@ -0,0 +1,18 @@
+package Variable;
+
+public class variable {
+    public static void main(String[] args) {
+        var a=3.14;
+        a=3.14555;
+
+        final float pie=3.14f;
+        // pie=3.14444f;
+
+        int x,y,z;
+        x=y=z=10;
+
+        
+        System.out.println(pie);
+        System.out.println(a);
+    }
+}
