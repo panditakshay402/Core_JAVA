b1c18df2086a6a3942fc12b6fdcb3ef9fe11b847
PanditAkshay402 <PanditAkshay402@gmail.com>
2026-04-25 21:56:51 +0530

    primitive data type

---
+++ Data_Types/Datatypes.java
@@ -0,0 +1,18 @@
+package Data_Types;
+
+class Datatypes {
+
+    public static void main(String[] args) {
+        int a=13131313;
+        float b=2.16443165464f;
+        double c=2.111111156443164f;
+        int d='A';
+        boolean e=true;
+        short f=26465;
+        long g=264645145;
+        byte h=-128;
+
+        System.out.println(h);
+
+    }
+}
