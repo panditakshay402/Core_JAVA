722dd295cec39a54d1d5af393de4ebb3421d18ba
PanditAkshay402 <PanditAkshay402@gmail.com>
2026-04-25 22:39:55 +0530

    Arithmetic operator

---
+++ Operators/operators.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class operators {
+    public static void main(String[] args) {
+        int a=2, b=3;
+
+        //Arithmetic Operators
+        int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        int f=a/b;        
+        int g=a%b;        
+        System.out.println(g);
+
+    }
+}
