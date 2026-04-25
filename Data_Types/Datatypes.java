22a3f6714dc83c0b6268ca25967303c868ed76f3
PanditAkshay402 <PanditAkshay402@gmail.com>
2026-04-25 22:04:49 +0530

    Non primitive types

--- Data_Types/Datatypes.java
+++ Data_Types/Datatypes.java
@@ -3,16 +3,23 @@ package Data_Types;
 class Datatypes {
 
     public static void main(String[] args) {
-        int a=13131313;
-        float b=2.16443165464f;
-        double c=2.111111156443164f;
-        int d='A';
-        boolean e=true;
-        short f=26465;
-        long g=264645145;
-        byte h=-128;
+        // Primitive DataTypes
+        int a = 13131313;
+        float b = 2.16443165464f;
+        double c = 2.111111156443164f;
+        int d = 'A';
+        boolean e = true;
+        short f = 26465;
+        long g = 264645145;
+        byte h = -128;
 
-        System.out.println(h);
+        // Non-Primitive DataTypes
+        String s = "Akshay";
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arr[i]);
+        }
 
     }
 }
